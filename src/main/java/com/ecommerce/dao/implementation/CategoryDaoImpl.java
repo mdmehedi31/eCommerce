9518e2061c05b7329cff95f03f79c3935c6c2a56
mdmehedi31 <mdmehedihasan.mh31@gmail.com>
2022-11-13 16:31:42 +0600

    Update Category Controller & add new jsp file

--- src/main/java/com/ecommerce/dao/implementation/CategoryDaoImpl.java
+++ src/main/java/com/ecommerce/dao/implementation/CategoryDaoImpl.java
@@ -6,9 +6,7 @@ import org.hibernate.Session;
 import org.hibernate.SessionFactory;
 import org.hibernate.query.Query;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Component;
 
-import org.springframework.stereotype.Repository;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
